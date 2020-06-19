7772797b9e01afa3b70cd0fb5977a7e5d2af4d10
Stuart Herbert <stuart@stuartherbert.com>
2020-06-19 08:59:14 +0100

    WIP: added skeleton for MediaType

---
+++ src/v1/MediaType/makeMediaType.ts
@@ -0,0 +1,68 @@
+//
+// Copyright (c) 2020-present Ganbaro Digital Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the
+//    distribution.
+// 3. Neither the names of the copyright holders nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    applyFunctionalOptions,
+    FunctionalOption,
+    OnErrorOptions,
+    SmartConstructor,
+    THROW_THE_ERROR,
+} from "@safelytyped/core-types";
+
+import { MediaType } from "./MediaType";
+
+/**
+ * `makeMediaType()` is a smart constructor. It verifies that the
+ * `input` contains valid MediaType data, by calling
+ * {@link mustBeMediaTypeData}.
+ *
+ * @category MediaType
+ * @param input
+ * This is the data we'll use to create the new MediaType
+ * @param onError
+ * If `input` fails validation, we'll pass an {@link AppError} to this.
+ * @param fnOpts
+ * These are user-supplied functional options.
+ * @returns
+ * The new MediaType object.
+ */
+export const makeMediaType: SmartConstructor<string, MediaType, OnErrorOptions, MediaType> = (
+    input: string,
+    {
+        onError = THROW_THE_ERROR
+    }: Partial<OnErrorOptions> = {},
+    ...fnOpts: FunctionalOption<MediaType, OnErrorOptions>[]
+): MediaType => applyFunctionalOptions(
+    new MediaType(input, { onError }),
+    { onError },
+    ...fnOpts
+);
